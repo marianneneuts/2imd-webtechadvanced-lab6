0c8c8cf11f75f5941eb0f651c00c17fe66f56112
Marianne <marianneneuts@gmail.com>
2022-03-26 16:47:26 +0100

    feat: update score input for live

--- public/javascripts/live.js
+++ public/javascripts/live.js
@@ -2,7 +2,7 @@ const primus = Primus.connect("/", {
     reconnect: {
         max: Infinity, // Number: The max delay before we try to reconnect.
         min: 500, // Number: The minimum delay before we try reconnect.
-        retries: 10 // Number: How many times we should try to reconnect.
+        retries: 100 // Number: How many times we should try to reconnect.
     }
 });
 
@@ -10,11 +10,11 @@ primus.on("data", data => {
     console.log("Joepie, er is data!");
     console.log(data);
 
-    let character = data.charactername;
+    let character = "." + data.charactername;
     let score = data.score;
 
     console.log(character);
     console.log(score)
 
-    let td = document.querySelector("td");
-}); 
+    document.querySelector(character).innerHTML = score;
+});
