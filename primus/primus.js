838691cdc209e7e4983db8d8b6d9b9c46521f501
Marianne <marianneneuts@gmail.com>
2022-03-26 14:45:42 +0100

    feat: save client library in javascripts folder

---
+++ primus/primus.js
@@ -0,0 +1,17 @@
+var Primus = require('primus');
+
+let go = (server) => {
+    const primus = new Primus(server, {/* options */});
+
+    // primus.save(__dirname +'/primuslib.js');
+
+    primus.on('connection', (spark) => {
+        console.log("✨");
+
+        /* spark.on('data', (data) => {
+            primus.write(data);
+        }); */
+    });
+};
+
+module.exports.go = go;
